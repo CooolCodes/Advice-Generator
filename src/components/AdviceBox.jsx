37351a62cb4e895b8340650958f722a85bbae415
Ebun <ebunlomoala@gmail.com>
2025-05-07 09:45:07 +0100

    mobile and desktop divider added

--- src/components/AdviceBox.jsx
+++ src/components/AdviceBox.jsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
 import divider from "../../public/assets/images/pattern-divider-mobile.svg";
+import desktopDivider from "../../public/assets/images/pattern-divider-desktop.svg";
 import dice from "../../public/assets/images/icon-dice.svg";
 
 const URL = "https://api.adviceslip.com/advice";
-
-const getRandomAdviceId = async () => {
-  const response = await fetch(URL);
-
-  const body = await response.json();
-  return body.slip.advice;
-};
 
 const getRandomAdvice = async () => {
   const response = await fetch(URL);
@@ -19,8 +13,8 @@ const getRandomAdvice = async () => {
 };
 
 const AdviceBox = () => {
-  const [adviceId, setAdviceId] = useState(null);
-  const [advice, setAdvice] = useState([]);
+  const [adviceId, setAdviceId] = useState();
+  const [advice, setAdvice] = useState();
 
   const getAdvice = async () => {
     const adviceData = await getRandomAdvice();
@@ -28,16 +22,18 @@ const AdviceBox = () => {
     setAdvice(adviceData.advice);
   };
   return (
-    <div className="bg-[hsl(218,23%,16%)] p-20">
-      <div className="bg-[hsl(217,19%,24%)] flex flex-col justify-center items-center gap-6  m-auto p-4 rounded-md w-100">
+    <div className="bg-[hsl(218,23%,16%)] h-dvh pt-20 lg:pt-50">
+      <div className=" bg-[hsl(217,19%,24%)] flex flex-col justify-center items-center gap-6 m-auto p-4 rounded-md w-[90%] lg:w-[40%]">
         <h1 className="text-[hsl(150,100%,66%)]"> Advice #{adviceId}</h1>
         <p className="text-2xl text-[hsl(193,38%,86%)] text-center">
           "{advice}"
         </p>
-        <img src={divider} alt="divider" />
+        <div
+          className={`bg-[url(${divider})] w-full h-auto bg-no-repeat bg-center lg:bg-[(${desktopDivider}))] lg:h-auto lg:w-full`}
+        ></div>
         <button
           onClick={getAdvice}
-          className="bg-[hsl(150,100%,66%)] p-4 rounded-full cursor-pointer hover:shadow-[1px_1px_15px_6px_rgb(82_255_168_/_1)]"
+          className="bg-[hsl(150,100%,66%)] p-4 rounded-full cursor-pointer hover:shadow-[1px_1px_15px_6px_rgb(82_255_168_/_1)] relative top-10"
         >
           <img src={dice} alt="dice" />
         </button>
