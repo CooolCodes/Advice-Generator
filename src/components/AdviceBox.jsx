f2dcad20dddcbb536fd64d32e2990c9a71ee4437
Ebun <ebunlomoala@gmail.com>
2025-05-16 16:01:40 +0100

    divider fix

--- src/components/AdviceBox.jsx
+++ src/components/AdviceBox.jsx
@@ -32,9 +32,7 @@ const AdviceBox = () => {
     };
   }, []);
   const getBackgroundImage = () => {
-    if (isMobile) {
-      return isMobile ? divider : desktopDivider;
-    }
+    return isMobile ? divider : desktopDivider;
   };
 
   const getAdvice = async () => {
@@ -49,10 +47,20 @@ const AdviceBox = () => {
         <p className="text-2xl text-[hsl(193,38%,86%)] text-center">
           "{advice}"
         </p>
+        {/* Mobile */}
         <div
+          className="h-64 bg-cover bg-center md:hidden"
+          style={{ backgroundImage: `url(${divider})` }}
+        ></div>
+        {/* Desktop */}
+        <div
+          className="hidden h-64 bg-cover bg-center md:block"
+          style={{ backgroundImage: `url(${desktopDivider})` }}
+        ></div>
+        {/* <div
           className={`bg-[url(${divider})] w-full h-auto bg-no-repeat bg-center lg:bg-[(${desktopDivider}))] lg:h-auto lg:w-full`}
           style={{ backgroundImage: `url(${getBackgroundImage()})` }}
-        ></div>
+        ></div> */}
         <button
           onClick={getAdvice}
           className="bg-[hsl(150,100%,66%)] p-4 rounded-full cursor-pointer hover:shadow-[1px_1px_15px_6px_rgb(82_255_168_/_1)] relative top-10"
